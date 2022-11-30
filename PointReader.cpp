4a4f4930e5e63144bf7db66d07d8d437f9dc270e
Danlif1 <danlif1302@gmail.com>
2022-11-30 21:37:07 +0200

    Added PointReader

---
+++ PointReader.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Daniel Lifshitz on 30/11/2022.
+//
+
+#include "PointReader.h"
+
+//std::vector<Point> ReadIrisFile(int argc, char** argv){}
---
+++ PointReader.h
@@ -0,0 +1,15 @@
+//
+// Created by Daniel Lifshitz on 30/11/2022.
+//
+
+#ifndef AP_TASK2_POINTREADER_H
+#define AP_TASK2_POINTREADER_H
+
+/**
+ * @brief Iris will be represented as points in space as it is the same and could be minimally changed to fit other discriptions.
+ * This comment will be moved afterwards to the Point file.
+ */
+//std::vector<Point> ReadIrisFile(int argc, char** argv);
+
+
+#endif //AP_TASK2_POINTREADER_H

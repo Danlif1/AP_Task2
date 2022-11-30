a4f905ee2025431678076258ff390e5db9de3ff9
Danlif1 <danlif1302@gmail.com>
2022-11-30 21:41:32 +0200

    Added getters

--- Point.cpp
+++ Point.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "Point.h"
+/**
+ * @brief Iris will be represented as points in space as it is the same and could be minimally changed to fit other discriptions.
+ */
+
+/**
+ * @brief Width getter.
+ * 
+ * @return double The width of the point.
+ */
+double Point::getWidth() const {
+    return width;
+}
+
+/**
+ * @brief Sepal length getter.
+ * 
+ * @return double The sepal length of the point.
+ */
+double Point::getSepalLength() const {
+    return sepalLength;
+}
+
+/**
+ * @brief Sepal width getter.
+ * 
+ * @return double The sepal width of the point.
+ */
+
+double Point::getSepalWidth() const {
+    return sepalWidth;
+}
+
+/**
+ * @brief Petal length getter.
+ * 
+ * @return double The petal length of the point.
+ */
+
+double Point::getPetalLength() const {
+    return petalLength;
+}
+
+/**
+ * @brief PointType getter.
+ * 
+ * @return PointType The point type (enum).
+ */
+pointType Point::getType() const {
+    return type;
+}
--- Point.h
+++ Point.h
@@ -5,12 +5,33 @@
 #ifndef AP_TASK2_POINT_H
 #define AP_TASK2_POINT_H
 
-
-
-class Point {
-
+//Called PointType so you could change the thing you try to classfiy/want to add more types.
+enum pointType
+{
+    setosa,
+    virginica,
+    versicolor,
+    UNKNOWN
 };
 
+class Point {
+    // We don't want the iris specific stats to be changed.
+    private:
+        double width;
+        double sepalLength;
+        double sepalWidth;
+        double petalLength;
+        pointType type;
+    public:
+        //TODO: Add Constractor/s for point.
+        //We do want to access the information of the iris so we will create getters.
+        double getWidth() const;
+        double getSepalLength() const;
+        double getSepalWidth() const;
+        double getPetalLength() const;
+        pointType getType() const;
+
+};
 
 
 #endif //AP_TASK2_POINT_H

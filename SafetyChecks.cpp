0eceff660dfd6cf7771633e096be7716234b5170
Danlif1 <danlif1302@gmail.com>
2022-11-30 22:31:55 +0200

    Added comments to saftey checks

--- SafetyChecks.cpp
+++ SafetyChecks.cpp
@@ -4,6 +4,13 @@
 
 #include "SafetyChecks.h"
 
+/**
+ * @brief checks that there is a K, file and a distance.
+ * 
+ * @param argc the amount of things given.
+ * @return true The amount is ok.
+ * @return false The amount is not ok.
+ */
 bool NumberOfArguments(int argc){
     if (argc == 4){
         return true;
@@ -18,6 +25,14 @@ bool NumberOfArguments(int argc){
 
 }
 
+/**
+ * @brief Checks the amount of points given and the amount of points needed for the KNN algorithm is ok.
+ * 
+ * @param k The amount of points of points needed for the KNN algorithm.
+ * @param amountOfPoints The amount of points given.
+ * @return true The K is ok.
+ * @return false The K is not ok.
+ */
 bool PointsCount(int k, int amountOfPoints){
     if (k > 0 && k < amountOfPoints){
         return true;

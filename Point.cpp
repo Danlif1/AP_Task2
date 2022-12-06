0bcb37c98cef65c3eb59df99055924b13a34e1d6
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2022-12-06 17:50:06 +0200

    Update Point.cpp
    Updated Point.cpp
    To fit to Point.h changes

--- Point.cpp
+++ Point.cpp
@@ -7,49 +7,65 @@
  * @brief Iris will be represented as points in space as it is the same and could be minimally changed to fit other discriptions.
  */
 
+
 /**
- * @brief Width getter.
+ * @brief Construct a new Point:: Point object using a vector and a type
  * 
- * @return double The width of the point.
+ * @param Stats A vector of information about the point.
+ * @param type The type of the point.
  */
-double Point::getWidth() const {
-    return width;
+Point::Point(std::vector<double> Stats, pointType type) 
+            : pointStats(Stats), type(type) {}
+
+/**
+ * @brief Construct a new Point:: Point object using a different point.
+ * 
+ * @param point The point we copy.
+ */
+Point::Point(const Point& point){
+    for (int i = 0; i < point.getAll().size(); i++){
+        this->pointStats[i] = point.getStat(i);
+    }
+    this->type = point.getType();
+}
+
+
+
+/**
+ * @brief Specific getter.
+ * Will return a specific stat in the point.
+ * for example: in (x,y,z) you could get x y or z.
+ * 
+ * @return double The stat of the point asked by the user.
+ */
+double Point::getStat(int place) const {
+    if(place >=0 && place <= pointStats.size()){
+        return pointStats[place];
+    }
+    else {
+        std::cout << "No stat like that exists please pick another number";
+        return NULL;
+    }
 }
 
 /**
- * @brief Sepal length getter.
+ * @brief Type getter.
  * 
- * @return double The sepal length of the point.
+ * @return pointType The type the point has.
  */
-double Point::getSepalLength() const {
-    return sepalLength;
-}
-
-/**
- * @brief Sepal width getter.
- * 
- * @return double The sepal width of the point.
- */
-
-double Point::getSepalWidth() const {
-    return sepalWidth;
-}
-
-/**
- * @brief Petal length getter.
- * 
- * @return double The petal length of the point.
- */
-
-double Point::getPetalLength() const {
-    return petalLength;
-}
-
-/**
- * @brief PointType getter.
- * 
- * @return PointType The point type (enum).
- */
-pointType Point::getType() const {
+pointType Point::getType() const{
     return type;
 }
+
+/**
+ * @brief Point getter.
+ * Will return the entire point.
+ * for example: with (x,y,z) you will get (x,y,z).
+ * 
+ * @return std::vector<double> The entire point.
+ */
+std::vector<double> Point::getAll() const {
+    return pointStats;
+}
+
+

66d534d0d1ae051905073545e224fb57b851f967
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2022-12-11 16:24:15 +0200

    Update ex2.cpp
    The start of main so it would read the file and start an infinite loop.

--- ex2.cpp
+++ ex2.cpp
@@ -2,13 +2,33 @@
 // Created by Daniel Lifshitz on 30/11/2022.
 //
 #include "SafetyChecks.h"
+#include "Point.h"
 #include <iostream>
 
 int main(int argc, char **argv) {
     if(NumberOfArguments(argc)){
-        //TODO: Read the file.
-        //TODO: Add Iris cpp/h file.
-        //TODO: Add a KNN calculating algorithm.
+        //Saving the K.
+        int k = std::stoi(argv[1]);
+        //Saving the file name.
+        std::string file = argv[2];
+        //Saving the distance metric.
+        std::string metric = argv[3];
+        //Create classified Iris vector.
+        PointReader classifiedPointReader(file);
+        Point cPoint;
+        std::vector<Point> classifiedPoints;
+        while (classifiedPointReader.getNextPoint(cPoint)) { classifiedPoints.push_back(cPoint); }
+        //Making an infinite loop to read points.
+        while (true) {
+            //sPoint = string point.
+            std::string sPoint;
+            std::cin << sPoint;
+            //uPoint = unclassified point.
+            Point uPoint;
+            uPoint.setFromVector(sPoint);
+            //TODO: Add a KNN calculating algorithm.
+        }
+        //DONE: Read the file.
         //TODO: Safety checks to the code.
         //TODO: Add explanations to the code.
     }

de782510b832d4268f05440b032dd75144e20865
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2022-12-12 22:13:02 +0200

    Update Point.h
    Added a few things and fixed a few bugs.

--- Point.h
+++ Point.h
@@ -2,31 +2,36 @@
 // Created by Daniel Lifshitz on 30/11/2022.
 //
 
-#ifndef AP_TASK2_POINT_H
-#define AP_TASK2_POINT_H
+#ifndef AP_Task2_POINT_H
+#define AP_Task2_POINT_H
 
 #include <vector>
 #include <iostream>
-#include "SafetyChecks.h"
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <fstream>
+
 //Called PointType so you could change the thing you try to classify/want to add more types.
 
 
 class Point {
     // We don't want the iris specific coordinates to be changed.
-    private:
-        std::vector<double> pointCords;
-        std::string type;
-    public:
-        Point(std::vector<double> = {0}, std::string type = "UNKNOWN");
-        Point(const Point& point);
-        //We want to be able to add as much information of the point as wanted.
-        double getCord(int) const;
-        std::string getType() const;
-        std::vector<double> getAll() const;
-        void setFromVector(std::vector<std::string>);
+private:
+    std::vector<double> pointCords;
+    std::string type;
+public:
+    Point(std::vector<double> = {0}, std::string type = "");
+    Point(const Point& point);
+    //We want to be able to add as much information of the point as wanted.
+    double getCord(int) const;
+    std::string getType() const;
+    std::vector<double> getAll() const;
+    void setFromVector(std::vector<std::string>);
+    void setFromString(std::string,char);
 };
-
+bool IsDouble(std::string);
 std::ostream& operator<<(std::ostream& os, const Point& point);
 
 
-#endif //AP_TASK2_POINT_H
+#endif //AP_Task2_POINT_H

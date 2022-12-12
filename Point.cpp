36916786a3c66d875cce9eb215bf14ba3c88c170
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2022-12-12 22:13:25 +0200

    Update Point.cpp
    Fixed a few bugs.

--- Point.cpp
+++ Point.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Point.h"
+using namespace std;
 /**
  * @brief Iris will be represented as points in space as it is the same and could be minimally changed to fit other descriptions.
  */
@@ -10,21 +11,21 @@
 
 /**
  * @brief Construct a new Point:: Point object using a vector and a type
- * 
+ *
  * @param Cords A vector of information about the point.
  * @param type The type of the point.
  */
 Point::Point(std::vector<double> Cords, std::string type)
-            : pointCords(Cords), type(type) {}
+        : pointCords(Cords), type(type) {}
 
 /**
  * @brief Construct a new Point:: Point object using a different point.
- * 
+ *
  * @param point The point we copy.
  */
 Point::Point(const Point& point){
     for (int i = 0; i < point.getAll().size(); i++){
-        this->pointCords[i] = point.getCord(i);
+        this->pointCords.push_back(point.getCord(i));
     }
     this->type = point.getType();
 }
@@ -35,7 +36,7 @@ Point::Point(const Point& point){
  * @brief Specific getter.
  * Will return a specific coordinate in the point.
  * for example: in (x,y,z) you could get x y or z.
- * 
+ *
  * @return double The coordinate of the point asked by the user.
  */
 double Point::getCord(int place) const {
@@ -44,13 +45,13 @@ double Point::getCord(int place) const {
     }
     else {
         std::cout << "No coordinate like that exists please pick another number";
-        return NULL;
+        return 0;
     }
 }
 
 /**
  * @brief Type getter.
- * 
+ *
  * @return pointType The type the point has.
  */
 std::string Point::getType() const{
@@ -61,7 +62,7 @@ std::string Point::getType() const{
  * @brief Point getter.
  * Will return the entire point.
  * for example: with (x,y,z) you will get (x,y,z).
- * 
+ *
  * @return std::vector<double> The entire point.
  */
 std::vector<double> Point::getAll() const {
@@ -71,18 +72,38 @@ std::vector<double> Point::getAll() const {
  * Make a point from a vector of the fields given in strings.
  * @param v The vector.
  */
-void Point::setFromVector(std::vector<std::string> v) {
-    // The first size()-2 of the vector arguments should be doubles representing the coordinates of the points.
-    for (int i = 0; i < v.size() - 1; i++){
-        if(/**isDouble(v[i]**/){
-            this->pointCords[i] = std::stod(v[i]);
+
+
+
+void Point::setFromString(std::string v, char spacer) {
+    std::vector<double> v2;
+    //using code from Task 1.
+    std::stringstream ss(v);
+    std::string s;
+    while (std::getline(ss, s, spacer)) {
+        if (IsDouble(s)) {
+            v2.push_back(stod(s));
         }
-        throw std::invalid_argument("All of the point coordinates should be numbers not strings");
+        else {
+            //This is the type.
+            this->type = s;
+        }
     }
-    this->type = v[v.size()-1];
+    this->pointCords = v2;
 }
 
-
+void Point::setFromVector(std::vector<std::string> Cords) {
+    this->pointCords.clear();
+    for (int i = 0; i < Cords.size() - 1; i++){
+        if (IsDouble(Cords[i])){
+            this->pointCords.push_back(std::stod(Cords[i]));
+        }
+        else {
+            throw std::invalid_argument("All of the coordinates of the points should be numbers");
+        }
+    }
+    this->type = Cords[Cords.size()-1];
+}
 
 /**
  * << operator for Point.
@@ -100,4 +121,18 @@ std::ostream &operator<<(std::ostream &os, const Point &point) {
     return os << output;
 }
 
+/**
+ * Checking if maybeDouble is a number.
+ * @param maybeDouble The string we got we want to confirm to a number.
+ * @return True if the first digit is a number; false otherwise.
+ */
+bool IsDouble(std::string maybeDouble){
+    int firstDigit = maybeDouble[0] - '0';
+    if (firstDigit >= 0 && firstDigit <= 9){
+        //The function stod(maybeDouble) will return a double even if there are letters in th number.
+        return true;
+    }
+    //The first digit is not a number thus stod(maybeDouble) will return an error and we do not want that to happen.
+    return false;
+}
 

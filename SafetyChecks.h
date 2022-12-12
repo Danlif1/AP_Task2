af640e4359a2602ea03260b4b250d9bf04f46b06
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2022-12-12 22:11:12 +0200

    Update SafetyChecks.h
    Added new safety checks signutres.

--- SafetyChecks.h
+++ SafetyChecks.h
@@ -2,12 +2,16 @@
 // Created by Daniel Lifshitz on 30/11/2022.
 //
 
-#ifndef AP_TASK2_SAFETYCHECKS_H
-#define AP_TASK2_SAFETYCHECKS_H
+#ifndef AP_Task2_SAFETYCHECKS_H
+#define AP_Task2_SAFETYCHECKS_H
 
 #include <iostream>
-
-bool NumberOfArguments(int argc);
-
-
-#endif //AP_TASK2_SAFETYCHECKS_H
+#include "Point.h"
+bool NumberOfArguments(int);
+bool PointsCount(long, long);
+bool KCheck(std::string);
+bool IsSameSize(std::vector<Point>);
+bool GoodVector(Point, Point);
+bool CheckFile (std::string const &);
+bool CheckMetric(std::string);
+#endif //AP_Task2_SAFETYCHECKS_H

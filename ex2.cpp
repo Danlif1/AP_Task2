da03a2775827f53496a9d68831acaa8501f0ff07
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2022-12-12 22:10:29 +0200

    Update ex2.cpp
    Added safety checks and fixed a few bugs.

--- ex2.cpp
+++ ex2.cpp
@@ -1,6 +1,4 @@
-//
-// Created by Daniel Lifshitz on 30/11/2022.
-//
+
 #include "SafetyChecks.h"
 #include "Point.h"
 #include "PointReader.h"
@@ -8,31 +6,66 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    if(NumberOfArguments(argc)){
+    std::fstream fout;
+
+    // opens an existing csv file or creates a new file.
+    fout.open("classified.csv", std::ios::out | std::ios::app);
+    if(NumberOfArguments(argc)) {
         //Saving the K.
-        int k = std::stoi(argv[1]);
+        int k;
+        if (KCheck(argv[1])) {
+            k = stoi(argv[1]);
+        } else {
+            //K is equal 0, so we need to terminate the program.
+            return 0;
+        }
         //Saving the file name.
         std::string file = argv[2];
+        if (!CheckFile(file)) {
+            //The file doesn't end with .csv so we need to terminate the program.
+            return 0;
+        }
         //Saving the distance metric.
         std::string metric = argv[3];
+        if (!CheckMetric(metric)) {
+            //The metric isn't one of the given 5 so we need to terminate the program.
+            return 0;
+        }
         //Create classified vector.
+
         PointReader classifiedPointReader(file);
         Point cPoint;
         std::vector<Point> classifiedPoints;
         //creating an array of classified points.
         while (classifiedPointReader.getNextPoint(cPoint)) { classifiedPoints.push_back(cPoint); }
+
+        if (!PointsCount(k, classifiedPoints.size())){
+            //K is either too big or too small so we need to terminate the program.
+            return 0;
+        }
+        if(!IsSameSize(classifiedPoints)){
+            cout << "not all vectors are the same size, please enter the vectors again." << std::endl;
+            return 0;
+        }
         //initializing a run of KNN
         KNN knn_run(k,metric);
         knn_run.fit(classifiedPoints);
         //Making an infinite loop to read points.
         while (true) {
             //sPoint = string point.
-            std::string sPoint;
-            std::cin >> sPoint;
-            //uPoint = unclassified point.
             Point uPoint;
-            //The types don't match here. this function takes a vector<string> and not a regular char*.
-            uPoint.setFromVector(sPoint);
+            do {
+                std::string sPoint;
+                getline(cin, sPoint);
+                //uPoint = unclassified point.
+                //The types don't match here. this function takes a vector<string> and not a regular char*.
+                uPoint.setFromString(sPoint, ' ');
+                if (!GoodVector(uPoint, classifiedPoints[0])){
+                    std::cout << "Please enter the correct amount of arguments into the vector." << std::endl;
+                }
+            } while (!GoodVector(uPoint, classifiedPoints[0]));
+
+
             //DONE: Add a KNN calculating algorithm.
             //print predicted label to screen.
             std::cout << knn_run.predict(uPoint);
@@ -40,8 +73,12 @@ int main(int argc, char **argv) {
 
         }
         //DONE: Read the file.
-        //TODO: Safety checks to the code.
+        //DONE: Safety checks to the code.
         //TODO: Add explanations to the code.
+    }
+    else {
+        std::cout << "Not all arguments are given please enter the K, file and distance metric." << std::endl;
     }
 
 }
+

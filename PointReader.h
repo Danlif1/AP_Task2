c499cb2658f83b4ece6197ce6be643efd381b287
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2022-12-12 22:12:04 +0200

    Update PointReader.h
    Changed a few things.

--- PointReader.h
+++ PointReader.h
@@ -1,22 +1,26 @@
 //
 // Created by Daniel Lifshitz on 30/11/2022.
 //
+
+
+#ifndef AP_Task2_POINTREADER_H
+#define AP_Task2_POINTREADER_H
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <vector>
 #include "Point.h"
-#ifndef AP_TASK2_POINTREADER_H
-#define AP_TASK2_POINTREADER_H
-
+#include "SafetyChecks.h"
 class PointReader {
 private:
     std::string fileName;
     std::ifstream fin;
     std::vector<std::string> row;
+
+
 public:
     PointReader(const std::string&);
     bool getNextPoint(Point&);
-};
 
-#endif //AP_TASK2_POINTREADER_H
+};
+#endif //AP_Task2_POINTREADER_H

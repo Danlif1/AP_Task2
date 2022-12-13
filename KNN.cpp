6c619f9125a12027af1e1b06ad82d16d1ef9cba4
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2022-12-13 15:35:12 +0200

    Update KNN.cpp
    Final upade to KNN.cpp

--- KNN.cpp
+++ KNN.cpp
@@ -1,23 +1,20 @@
 
-#include <tuple>
-#include <algorithm>
 #include "KNN.h"
-#include "Point.h"
 
 /**
- * constructor
- * @param k
- * @param metric
+ * Constructor
+ * @param k The number of neighbours we want to find.
+ * @param metric The distance metric we want to use.
  */
-KNN::KNN(int k, string metric) {
+KNN::KNN(int k, std::string metric) {
     KNN::k = k;
     KNN::distance_metric = metric;
 }
 
 //point might not need to be a class of its own, if it is it's only to separate the vector from the label
 /**
- * fits the training data to the knn object. training data consists of data(x) and labels(y).
- * @param classified_points - vector that contains classified data
+ * Fits the training data to the knn object. training data consists of data(x) and labels(y).
+ * @param classified_points Vector that contains classified data.
  */
 void KNN::fit(std::vector<Point> classified_points) {
     //need to decide if we store the data as to separate arrays. might be better to utilize the class point.
@@ -26,22 +23,22 @@ void KNN::fit(std::vector<Point> classified_points) {
 }
 
 /**
- * predicts the label of a given unclassified vector
- * @return
+ * Predicts the label of a given unclassified vector.
+ * @return The label of the point.
  */
-string KNN::predict(Point newpoint) {
-    vector<tuple<double, string>> dis_from_point = distance(newpoint.getAll());
-    string label = nearestNeighbor(dis_from_point);
+std::string KNN::predict(Point newpoint) {
+    std::vector<std::tuple<double, std::string>> dis_from_point = distance(newpoint.getAll());
+    std::string label = nearestNeighbor(dis_from_point);
     return label;
 }
 
 /**
- * calculates the distance from the unclassified vector, to the classified data we have
- * @param a the unclassified information
- * @return a vector of tuples (the tuple will consist of the distance and the classified label) with the distance of the unclassified data from the classified vector
+ * Calculates the distance from the unclassified vector, to the classified data we have.
+ * @param a The unclassified information.
+ * @return A vector of tuples (the tuple will consist of the distance and the classified label) with the distance of the unclassified data from the classified vector.
  */
-vector<tuple<double, string>> KNN::distance(vector<double> a) {
-    vector<tuple<double, string>> dis_from_point;
+std::vector<std::tuple<double, std::string>> KNN::distance(std::vector<double> a) {
+    std::vector<std::tuple<double, std::string>> dis_from_point;
     double tmp;
     for (int i = 0; i < KNN::data.size(); ++i) {
         //add calcDistance to distance file
@@ -52,21 +49,21 @@ vector<tuple<double, string>> KNN::distance(vector<double> a) {
 }
 
 /**
- * receives an array of distances and finds the max label that appears
- * @param distances the vector obtained from the distance method.
- * @return the label that appears most in the distances vector
+ * Receives an array of distances and finds the max label that appears.
+ * @param distances Rhe vector obtained from the distance method.
+ * @return The label that appears most in the distances vector
  */
-string KNN::nearestNeighbor(vector<tuple<double, string>> distances) const {
+std::string KNN::nearestNeighbor(std::vector<std::tuple<double, std::string>> distances) const {
     //sort distances based on first element in ascending order
-    std::sort(distances.begin(), distances.end(), greater<>());
-    vector<string> label_of_dis;
-    string most_common;
+    std::sort(distances.begin(), distances.end());
+    std::vector<std::string> label_of_dis;
+    std::string most_common;
     for (int i = 0; i < k; ++i) {
         label_of_dis.emplace_back(get<1>(distances[i]));
     }
     sort(label_of_dis.begin(), label_of_dis.end());
-    string check = label_of_dis[0];
-    string max_label;
+    std::string check = label_of_dis[0];
+    std::string max_label = check;
     int count = 0;
     int max = 0;
     for (int i = 0; i < label_of_dis.size(); ++i) {
